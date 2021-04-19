3c3f8f039efc6b477f8b2bd32dddc49e363a04ee
Alisson Andrade <alisson.piris@wdna.com.br>
2021-04-19 14:01:13 -0300

    Service de Consulta

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app     = express();
 const bodyParser = require("body-parser");
 const mongoose   = require("mongoose");
+const appointmentService = require("./services/AppointmentService");
+
 
 app.use(express.static("public"));
 
@@ -13,9 +15,29 @@ app.set("view engine", "ejs");
 mongoose.connect("mongodb://localhost:27017/agendamento",{ useNewUrlParser: true, useUnifiedTopology: true})
 
 app.get("/",(req,res) => {
+    res.send("Oi deu certo")
+})
+
+app.get("/cadastro", (req, res) => {
     res.render("create")
 })
 
-app.listen(5000,() => {
+app.post("/create", async(req,res) => {
+    var status = await appointmentService.Create(
+        req.body.name,
+        req.body.email,
+        req.body.cpf,
+        req.body.description,
+        req.body.date,
+        req.body.time,
+    )
+    if(status){
+        res.redirect("/");
+    } else {
+        res.send("Ocorreu uma falha!")        
+    }
+})
+
+app.listen(8080,() => {
     console.log("Rodando Servidor Agendamento")
 })
